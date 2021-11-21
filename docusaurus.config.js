39ef9548805261613f0aaff71b9f59dab217bb16
John Hughes <johnhughes1984@icloud.com>
2021-11-21 07:57:39 +0000

    Supported by Fisheye link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} MageQuest. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} MageQuest. Supported by <a href="https://www.fisheye.co.uk/"><img src="/img/fisheye-logo.png" alt="Fisheye"/></a>`,
       },
       prism: {
         theme: lightCodeTheme,
